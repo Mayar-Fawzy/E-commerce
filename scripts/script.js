3e5bcfe284a1f40df81c7b658ece6674cec00526
Mayar fawzy <142163339+Mayar-Fawzy@users.noreply.github.com>
2024-09-18 05:56:08 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+let countHeart=0;
+
+function Hearts(){
+     countHeart++;
+    document.getElementById("NumberOfFavourite").innerHTML=countHeart;
+}
+let countShoping=0;
+
+function Shoping(){
+     countShoping++;
+    document.getElementById("NumberOfShoping").innerHTML=countShoping;
+}
+
